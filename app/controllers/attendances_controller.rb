a29eca92dd3ef89a99aadc6bc661a2c6b34780cf
ryuki-o <ap023otakeyama0503@outlook.jp>
2021-02-01 14:42:35 +0000

    Finish add 1

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -35,14 +35,15 @@ class AttendancesController < ApplicationController
         attendance = Attendance.find(id)
         attendance.update_attributes!(item)
       end
-    end
+  end
+    
     flash[:success] = "1ヶ月分の勤怠情報を更新しました。"
     redirect_to user_url(date: params[:date])
   rescue ActiveRecord::RecordInvalid # トランザクションによるエラーの分岐です。
     flash[:danger] = "無効な入力データがあった為、更新をキャンセルしました。"
     redirect_to attendances_edit_one_month_user_url(date: params[:date])
   end
-
+  
   private
 
     # 1ヶ月分の勤怠情報を扱います。

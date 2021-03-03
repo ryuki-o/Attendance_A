d3ee9e01361ffa8f729981f22fb1d4b2c1fd95f1
ryuki-o <ap023otakeyama0503@outlook.jp>
2021-03-03 14:50:34 +0000

    update all

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -33,19 +33,17 @@ class AttendancesController < ApplicationController
     ActiveRecord::Base.transaction do # トランザクションを開始します。
       attendances_params.each do |id, item|
         attendance = Attendance.find(id)
-        attendance.update_attributes!(item)
-      
-
+        attendance.attributes = item
+        attendance.save!(context: :attendance_update)
       end
-  end
-    
+    end
     flash[:success] = "1ヶ月分の勤怠情報を更新しました。"
     redirect_to user_url(date: params[:date])
   rescue ActiveRecord::RecordInvalid # トランザクションによるエラーの分岐です。
     flash[:danger] = "無効な入力データがあった為、更新をキャンセルしました。"
     redirect_to attendances_edit_one_month_user_url(date: params[:date])
   end
-  
+
   private
 
     # 1ヶ月分の勤怠情報を扱います。
--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -9,12 +9,8 @@ class Attendance < ApplicationRecord
   # 出勤・退勤時間どちらも存在する時、出勤時間より早い退勤時間は無効
   validate :started_at_than_finished_at_fast_if_invalid
   
-  validate :started_at_is_invalid_without_a_finished_at, on: :update_one_month
+  validate :started_at_is_invalid_without_a_finished_at, on: :attendance_update
   
-   with_options on: :update_one_month? do
-    validate :started_at_is_invalid_without_a_finished_at
-  end
-
   def finished_at_is_invalid_without_a_started_at
     errors.add(:started_at, "が必要です") if started_at.blank? && finished_at.present?
   end
@@ -26,7 +22,7 @@ class Attendance < ApplicationRecord
   end
   
   def started_at_is_invalid_without_a_finished_at
-    errors.add(:finished_at, "が必要です") if started_at.present? && finished_at.blank?(context: :pattern_validation)
+    errors.add(:finished_at, "が必要です") if started_at.present? && finished_at.blank?
   end
   
 end
